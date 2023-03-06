7edc12b5da8a79e3e52dcede25f6fa0db65300b1
mikaellecosta <mikahcosta12@gmail.com>
2023-03-06 19:53:38 -0300

    06.03.23

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,57 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
 
 main() {
-  runApp(new Game());
+  runApp(PerguntaApp());
 }
 
-class Game extends StatelessWidget {
-  Widget build(BuildContext context) {
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
 
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;
+    });
+    print('Pergunta respondida!');
+  }
+
+  Widget build(BuildContext context) {
     final perguntas = [
-      'What is your name?'
-      'How old are you?'
+      'Qual é o seu nome?',
+      'Qual sua idade?',
     ];
 
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Mika Quest-App"),
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Quiz App - Mika"),
       ),
-      body: Column(
-        children: <Widget>[
-          Text(perguntas[0]),
-          ElevatedButton(
-            child: Text('Answer 1'),
-            onPressed: null,
-          ),
-        ]
-      ),
-    )
-  );
+      body: Column(children: <Widget>[
+        Questao(perguntas[_perguntaSelecionada]),
+
+        ElevatedButton(
+          child: Text('Resposta 1'),
+          onPressed: _responder,
+        ),
+        ElevatedButton(
+          child: Text('Resposta 2'),
+          onPressed: _responder,
+        ),
+        ElevatedButton(
+          child: Text('Resposta 3'),
+          onPressed: _responder,
+        ),
+        ElevatedButton(
+          child: Text('Resposta 4'),
+          onPressed: _responder,
+        ),
+      ]),
+    ));
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
   }
 }
